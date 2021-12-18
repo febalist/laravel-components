350dcea0bb685605913d7415820a7485b4a80949
febalist <febalist@gmail.com>
2021-12-18 17:49:40 +0200

    components class fix

--- views/components/input-group.blade.php
+++ views/components/input-group.blade.php
@@ -1,23 +1,23 @@
-<div class="input-group">
-    @if(isset($prepend) || isset($prefix))
-        <div class="input-group-prepend">
-            @if($prefix)
-                <span class="input-group-text">{{ $prefix }}</span>
-            @else
-                {{ $prepend }}
-            @endif
-        </div>
-    @endif
+<div {{ $attributes->merge(['class' => 'input-group']) }}>
+  @if(isset($prepend) || isset($prefix))
+    <div class="input-group-prepend">
+      @if($prefix)
+        <span class="input-group-text">{{ $prefix }}</span>
+      @else
+        {{ $prepend }}
+      @endif
+    </div>
+  @endif
 
-    {{ $slot }}
+  {{ $slot }}
 
-    @if(isset($append) || isset($suffix))
-        <div class="input-group-append">
-            @if($suffix)
-                <span class="input-group-text">{{ $suffix }}</span>
-            @else
-                {{ $append }}
-            @endif
-        </div>
-    @endif
+  @if(isset($append) || isset($suffix))
+    <div class="input-group-append">
+      @if($suffix)
+        <span class="input-group-text">{{ $suffix }}</span>
+      @else
+        {{ $append }}
+      @endif
+    </div>
+  @endif
 </div>
